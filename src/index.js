6d3cdab77f32544cea83ba896b66001f1bfbfd14
Alex Zdch <pachtouka@gmail.com>
2020-10-11 22:21:10 +0300

    feat: add function decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    function decodSymb(arr){
+
+        let strTemp = '';
+        let i = 0;
+        
+        if (arr[0] === '*') {
+   
+           return ' ';
+        }
+        do {
+  
+         if( arr[i] + arr[i+1] === '10')
+  
+         {
+             strTemp = strTemp + '.';
+          } else if ( arr[i] + arr[i+1] === '11' ) {
+  
+             strTemp = strTemp + '-';
+          }
+  
+           i = i + 2;
+        } while ( i < arr.length )
+  
+       return MORSE_TABLE[strTemp];
+  
+    }
+
+    let strArr = expr.split('');
+    let strResult = '';
+ 
+    while ( strArr.length ) 
+    {
+ 
+       strResult = strResult + decodSymb( strArr.splice(0,10) ); 
+ 
+    }
+ 
+ return strResult;    
+         
 }
 
 module.exports = {
